809630d26b5ea7050e9397d8a66a2d1cda1ec7b9
khushi10aug <khushi10aug@gmail.com>
2026-04-02 14:04:00 +0530

    head tag script updated

--- application/views/_partial/header/commonHeadBottom.php
+++ application/views/_partial/header/commonHeadBottom.php
@@ -44,34 +44,6 @@
 }
 </script>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17334097739"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-17334097739');
-</script>
-
-<!--script async src="https://www.googletagmanager.com/gtag/js?id=G-6V8RJDRWC9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-6V8RJDRWC9');
-</script-->
-
-
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TRJDHX7N');</script>
-<!-- End Google Tag Manager -->
-
 </head>
 <?php
 $bodyClass = ($controllerName == 'Home') ? 'home' : 'inner';
@@ -88,10 +60,6 @@ if (CommonHelper::demoUrl()) {
 ?>
 
 <body class="<?php echo $bodyClass; ?> ">
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRJDHX7N"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
     <script>
         <?php
         if (Message::getInfoCount() > 0 || Message::getDialogCount() > 0) { ?>

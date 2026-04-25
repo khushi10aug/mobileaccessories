bc34dae88c48ff3f1e4708f8be16b148feb28a10
khushi10aug <khushi10aug@gmail.com>
2026-04-25 16:31:54 +0530

    varient product

--- application/controllers/ProductsController.php
+++ application/controllers/ProductsController.php
@@ -976,7 +976,10 @@ class ProductsController extends MyAppController
             $optionSrchObj->joinTable(SellerProduct::DB_TBL_SELLER_PROD_OPTIONS, 'LEFT OUTER JOIN', 'selprod_id = tspo.selprodoption_selprod_id', 'tspo');
             $optionSrchObj->joinTable(OptionValue::DB_TBL, 'LEFT OUTER JOIN', 'tspo.selprodoption_optionvalue_id = opval.optionvalue_id', 'opval');
             $optionSrchObj->joinTable(Option::DB_TBL, 'LEFT OUTER JOIN', 'opval.optionvalue_option_id = op.option_id', 'op');
-            if (FatApp::getConfig('CONF_ENABLE_SELLER_SUBSCRIPTION_MODULE', FatUtility::VAR_INT, 0)) {
+            if (
+                FatApp::getConfig('CONF_ENABLE_SELLER_SUBSCRIPTION_MODULE', FatUtility::VAR_INT, 0) &&
+                FatUtility::int($product['selprod_user_id']) !== 1
+            ) {
                 $validDateCondition = " and oss.ossubs_till_date >= '" . date('Y-m-d') . "'";
                 $optionSrchObj->joinTable(Orders::DB_TBL, 'INNER JOIN', 'o.order_user_id=selprod_user_id AND o.order_type=' . ORDERS::ORDER_SUBSCRIPTION . ' AND o.order_payment_status =1', 'o');
                 $optionSrchObj->joinTable(OrderSubscription::DB_TBL, 'INNER JOIN', 'o.order_id = oss.ossubs_order_id and oss.ossubs_status_id=' . FatApp::getConfig('CONF_DEFAULT_SUBSCRIPTION_PAID_ORDER_STATUS') . $validDateCondition, 'oss');
@@ -992,6 +995,7 @@ class ProductsController extends MyAppController
             $optionSrch->addGroupBy('option_id');
 
             $optionRs = $optionSrch->getResultSet();
+            
             if (true === MOBILE_APP_API_CALL) {
                 $optionRows = FatApp::getDb()->fetchAll($optionRs);
             } else {

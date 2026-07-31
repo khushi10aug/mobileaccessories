0af3b99a971ee89a6c48e194bb6c80ac0a0f262d
khushi10aug <khushi10aug@gmail.com>
2026-07-31 12:19:49 +0530

    contact form

--- application/views/custom/contact-us.php
+++ application/views/custom/contact-us.php
@@ -209,6 +209,5 @@ $fld->developerTags['col'] = 12;
 $siteKey = FatApp::getConfig('CONF_RECAPTCHA_SITEKEY', FatUtility::VAR_STRING, '');
 $secretKey = FatApp::getConfig('CONF_RECAPTCHA_SECRETKEY', FatUtility::VAR_STRING, '');
 if (!empty($siteKey) && !empty($secretKey)) { ?>
-    <script defer
-        src='https://www.google.com/recaptcha/api.js?onload=googleCaptcha&render=<?php echo $siteKey; ?>'></script>
+    <script src='https://www.google.com/recaptcha/api.js?onload=googleCaptcha&render=<?php echo trim($siteKey); ?>'></script>
 <?php } ?>
